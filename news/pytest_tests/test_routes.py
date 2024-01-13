f1e94a648e75a5fc32db8a3e8b2c29be65095628
Denis Smagin <assbestmail@gmail.com>
2024-01-13 13:15:55 +0300

    Update test_routes.py
    Add tests

--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -2,6 +2,39 @@ from http import HTTPStatus
 
 import pytest
 from django.urls import reverse
+from pytest_django.asserts import assertRedirects
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('news:detail', pytest.lazy_fixture('news_id')),
+        ('news:home', None),
+        ('users:login', None),
+        ('users:logout', None),
+        ('users:signup', None),
+    )
+)
+def test_pages_availability_for_anonymous_user(client, name, args):
+    url = reverse(name, args=args)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('news:edit', pytest.lazy_fixture('comment_id')),
+        ('news:delete', pytest.lazy_fixture('comment_id')),
+    ),
+)
+def test_redirect_for_anonymous_client(client, name, args):
+    login_url = reverse('users:login')
+    url = reverse(name, args=args)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assertRedirects(response, expected_url)
 
 
 @pytest.mark.parametrize(
@@ -14,18 +47,13 @@ from django.urls import reverse
 @pytest.mark.parametrize(
     'name, args',
     (
-            ('news:edit', pytest.lazy_fixture('comment_id')),
-            ('news:delete', pytest.lazy_fixture('comment_id')),
+        ('news:edit', pytest.lazy_fixture('comment_id')),
+        ('news:delete', pytest.lazy_fixture('comment_id')),
     )
 )
-def test_availability_for_comment_edit_and_delete(parametrized_client, expected_status, name, args):
+def test_availability_for_comment_edit_and_delete(
+        parametrized_client, expected_status, name, args
+):
     url = reverse(name, args=args)
     response = parametrized_client.get(url)
     assert response.status_code == expected_status
-
-
-# def test_pages_availability(self):
-#     pass
-#
-# def test_redirect_for_anonymous_client(self):
-#     pass
